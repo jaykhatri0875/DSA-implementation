d4ee9b300a77e2f31cd2875bb8c7fbed58efe8a8
jaykhatri0875 <jay.khatri.0875@ldce.ac.in>
2022-03-05 13:06:05 +0530

    sliding window two sum added

--- 18_max_subsum.py
+++ 18_max_subsum.py
@@ -2,10 +2,24 @@
 
 # maximum subarray sum of size k
 
+from re import L
+
+# without 2 pointers
 def maxsum(array,k):
-    pass
+    sum = 0
+    maxsum = 0
+    l = len(array)
+    for i in range(l):
+        if(i+k<=l):
+            for j in range(i,i+k):
+                sum += array[j]
+                if(sum>maxsum):
+                    maxsum = sum
+            #print(sum)
+            sum = 0
+    return maxsum 
 
 if  __name__=='__main__':
-    array = [1,2,3,4,5,6,7]
+    array = [111,2,3,4,5,6,7]
     k = 3
     print(maxsum(array,k))
