3421cedad4cf3cd0241dd12ed8ffb6c32822ac22
jaykhatri0875 <jay.khatri.0875@ldce.ac.in>
2022-03-16 18:06:41 +0530

    more added

--- 21_opposite_pointers.py
+++ 21_opposite_pointers.py
@@ -1,1 +1,27 @@
 # opposite directional two pointers
+# we can reverse an array or string withour extra space
+# 
+def reverse(array):
+    i,j=0,len(array)-1
+    while(i<j):
+        array[i],array[j] = array[j],array[i]
+        i+=1
+        j-=1
+    return array
+# given sorted array and targetsum k,find two numbers such that they add up to k
+def twoSum(array,k):
+    i,j=0,len(array)-1
+    while(i<j):
+        sum = array[i] + array[j]
+        if(k==sum):
+            return [array[i],array[j]]
+        elif(k>sum):
+            i +=1
+        else:
+            j-=1
+    return []
+if __name__=="__main__":
+    array = [1,2,3,4,5,6,7,8]
+    k = 5
+    string = 'jayk'
+    print(twoSum(array,k))  
---
+++ 22_Three_pointers.py
@@ -0,0 +1,1 @@
+# in array of 0s and 1s, how many non empty subarrays have sum S?
